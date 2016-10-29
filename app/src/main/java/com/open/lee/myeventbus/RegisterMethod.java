63a329a625d997f11c26998119ab07178e7c9a8e
FrankLee96 <530304349@qq.com>
2016-10-29 19:41:18 +0800

    1.0.1 commit

--- app/src/main/java/com/open/lee/myeventbus/RegisterMethod.java
+++ app/src/main/java/com/open/lee/myeventbus/RegisterMethod.java
@@ -17,6 +17,15 @@ public class RegisterMethod {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((method == null) ? 0 : method.hashCode());
+        result = prime * result + ((threadMode == null) ? 0 : threadMode.hashCode());
+        return result;
+    }
+
+    @Override
     public boolean equals(Object obj) {
         if(this == obj)
             return true;
